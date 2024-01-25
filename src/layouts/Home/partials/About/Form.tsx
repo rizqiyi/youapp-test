b34aeb51e23b5f6e39890ed1c9dbe5b20ffccdf4
Rizqiyanto Imanullah <human.gesture@gmail.com>
2024-01-25 16:57:10 +0700

    feat: handle upload file and preview file

--- src/layouts/Home/partials/About/Form.tsx
+++ src/layouts/Home/partials/About/Form.tsx
@@ -1,12 +1,60 @@
+"use client";
+
+import React, { ChangeEvent, useState } from "react";
 import Input from "@/components/Input";
 import Image from "next/image";
-import React from "react";
 
 const Form = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+
+    setFile(file);
+
+    // Read the file and set the preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        setPreview(reader.result);
+      }
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <div>
       <div className="flex gap-[15px] items-center mb-[29px]">
-        <Image src="/icons/upload-ic.svg" width={57} height={57} alt="upload" />
+        <label htmlFor="upload">
+          <span aria-hidden="true">
+            {preview && (
+              <img
+                src={preview}
+                alt="Preview"
+                className="w-[57px] h-[57px] rounded-[17px] object-cover"
+              />
+            )}
+
+            {(!preview || !file) && (
+              <Image
+                src={"/icons/upload-ic.svg"}
+                width={57}
+                height={57}
+                alt="upload"
+              />
+            )}
+          </span>
+          <input
+            onChange={handleFileChange}
+            type="file"
+            id="upload"
+            className="hidden"
+          />
+        </label>
         <h6 className="text-[12px] leading-normal text-white">Add image</h6>
       </div>
       <div className="flex flex-col gap-3">
